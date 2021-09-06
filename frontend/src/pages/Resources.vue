95bc63dbed3c3eaa70df0f257c447320a8de766a
Tyler Young <tylerparkeyoung@gmail.com>
2021-09-05 23:06:25 -0700

    Color change
    color change and Spelling fix

--- frontend/src/pages/Resources.vue
+++ frontend/src/pages/Resources.vue
@@ -7,7 +7,7 @@
             <ul class="hidden md:flex items-center justify-between flex border-b border-gray-100 border-opacity-20 pb-4 mb-12">
                <li><a href="#token">Token</a></li>
                <li><a href="#rarity">Rarity Tools</a></li> 
-               <li><a href="#derivatives">Derivaties</a></li> 
+               <li><a href="#derivatives">Derivatives</a></li> 
                <li><a href="#markets">Markets</a></li> 
                <li><a href="#tools">Dev Tools</a></li> 
             </ul>
@@ -18,37 +18,37 @@
                     <h2 class="md:text-4xl">$NEURO</h2>
                     <h3>Gear Governance Token</h3>
                     <ul class="pt-5">
-                        <li><a class="text-purple-500" href="https://etherscan.io/address/0xe71946b31bda22526b2de702bca8b457ef85c4f1">Ethereum Contract</a> </li>
-                        <li><a class="text-purple-500" href="https://twitter.com/NeuroCredits">Twitter</a></li>
+                        <li><a class="text-green-600" href="https://etherscan.io/address/0xe71946b31bda22526b2de702bca8b457ef85c4f1">Ethereum Contract</a> </li>
+                        <li><a class="text-green-600" href="https://twitter.com/NeuroCredits">Twitter</a></li>
                     </ul>
                 </div>
                 <!-- Rarity Tools -->
                 <div id="rarity" class="mb-12 border-b pb-12">
                     <h2 class="md:text-4xl">Rarity Tools</h2>
                     <ul class="pt-5">
-                        <li><a class="text-purple-500" href="https://www.gearforpunks.app/">Rarity Display</a></li>
-                        <li><a class="text-purple-500" href="https://gist.github.com/Zen35/d25a130e384c9fb0e515d922fc977ce8">Deal Stealer" Neural Optimizer + 1</a></li>
-                        <li><a class="text-purple-500" href="https://docs.google.com/spreadsheets/d/1FrV0dLD4LxphhjCJ8qJ3H9Q05wrEdoVvgfleVfHT00E/edit#gid=0">Rarity List</a></li>
+                        <li><a class="text-green-600" href="https://www.gearforpunks.app/">Rarity Display</a></li>
+                        <li><a class="text-green-600" href="https://gist.github.com/Zen35/d25a130e384c9fb0e515d922fc977ce8">Deal Stealer" Neural Optimizer + 1</a></li>
+                        <li><a class="text-green-600" href="https://docs.google.com/spreadsheets/d/1FrV0dLD4LxphhjCJ8qJ3H9Q05wrEdoVvgfleVfHT00E/edit#gid=0">Rarity List</a></li>
                     </ul>
                 </div>
                 <!-- Derivatives -->
                 <div id="derivatives" class="mb-12 border-b pb-12">
-                    <h2 class="md:text-4xl">Derivaties</h2>
+                    <h2 class="md:text-4xl">Derivatives</h2>
                     <ul class="pt-5">
-                        <li><a class="text-purple-500" href="https://etherscan.io/address/0x985E5b415dE11fC9C884DdCEaAaC657853044622#writeContract">PunkStats</a> </li>
-                        <li><a class="text-purple-500" href="https://etherscan.io/address/0x10e7bCDD0822400bEE5c591a0BD818AD6F65c04f">Gear University</a></li>
+                        <li><a class="text-green-600" href="https://etherscan.io/address/0x985E5b415dE11fC9C884DdCEaAaC657853044622#writeContract">PunkStats</a> </li>
+                        <li><a class="text-green-600" href="https://etherscan.io/address/0x10e7bCDD0822400bEE5c591a0BD818AD6F65c04f">Gear University</a></li>
                     </ul>
                 </div>
                 <!-- Markets -->
                 <div id="markets" class="mb-12 border-b pb-12">
                     <h2 class="md:text-4xl">Markets</h2>
                     <ul class="pt-5">
-                        <li><a class="text-purple-500" href="https://punkaf.market/">Hoodies</a></li> 
-                        <li><a class="text-purple-500" href="https://www.mirrorshades.me/">Sunglasses</a></li>
-                        <li><a class="text-purple-500" href="https://1337.punkaf.market/">1337 Gear Market (+1 Elite Gear)</a></li>
-                        <li><a class="text-purple-500" href="https://rare.punkaf.market/">Rare AF Market</a></li>
-                        <li><a class="text-purple-500" href="https://holo.punkaf.market/">Holo hands Market</a></li>
-                        <li><a class="text-purple-500" href="https://katanas.punkaf.market/">Katana Market</a></li>
+                        <li><a class="text-green-600" href="https://punkaf.market/">Hoodies</a></li> 
+                        <li><a class="text-green-600" href="https://www.mirrorshades.me/">Sunglasses</a></li>
+                        <li><a class="text-green-600" href="https://1337.punkaf.market/">1337 Gear Market (+1 Elite Gear)</a></li>
+                        <li><a class="text-green-600" href="https://rare.punkaf.market/">Rare AF Market</a></li>
+                        <li><a class="text-green-600" href="https://holo.punkaf.market/">Holo hands Market</a></li>
+                        <li><a class="text-green-600" href="https://katanas.punkaf.market/">Katana Market</a></li>
                         
                     </ul>
                 </div>
@@ -56,10 +56,10 @@
                 <div id="tools" class="mb-12 pb-12">
                     <h2 class="md:text-4xl">Dev Tools</h2>
                     <ul class="pt-5">
-                        <li><a class="text-purple-500" href="https://docs.google.com/spreadsheets/d/1FrV0dLD4LxphhjCJ8qJ3H9Q05wrEdoVvgfleVfHT00E/edit#gid=0">Rarity List</a></li> 
-                        <li><a class="text-purple-500" href="http://147.135.115.140/home/ubuntu/gear.json">Full JSON Data</a></li>
-                        <li><a class="text-purple-500 text-wrap" href="https://github.com/Gear-for-Punks/dhof-gear/tree/master/output">Stashes, Occurrences, Rarity JSON</a></li>
-                        <li><a class="text-purple-500" href="https://docs.google.com/spreadsheets/d/1CEobE011Q0fSugwFR9EapUM_2qTq1i7b5JgFFf9Jhus/edit#gid=0">Full Gear list</a></li>
+                        <li><a class="text-green-600" href="https://docs.google.com/spreadsheets/d/1FrV0dLD4LxphhjCJ8qJ3H9Q05wrEdoVvgfleVfHT00E/edit#gid=0">Rarity List</a></li> 
+                        <li><a class="text-green-600" href="http://147.135.115.140/home/ubuntu/gear.json">Full JSON Data</a></li>
+                        <li><a class="text-green-600 text-wrap" href="https://github.com/Gear-for-Punks/dhof-gear/tree/master/output">Stashes, Occurrences, Rarity JSON</a></li>
+                        <li><a class="text-green-600" href="https://docs.google.com/spreadsheets/d/1CEobE011Q0fSugwFR9EapUM_2qTq1i7b5JgFFf9Jhus/edit#gid=0">Full Gear list</a></li>
                     </ul>
                 </div>
             </div>
